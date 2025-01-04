2547c1ef6e5bb4cdff095b892756ad17a0fb3dee
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-04 15:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element = document.getElementById('level');
+let level = 0;
+while (element) {
+    level++;
+    element = element.parentNode;
+}
+alert('The level of the element is: ' + level);
